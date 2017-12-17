ad845f6630ed0ea29acf0b65a9075a69518f5489
Ubuntu <ubuntu@ip-172-31-83-196.ec2.internal>
2017-12-17 00:35:54 +0000

    change batch size for rnn

--- experiments.sh
+++ experiments.sh
@@ -38,21 +38,21 @@ python clean_code/main.py  --onlybin False --captions True --augment True --epoc
 
 # R U NAN
 
-python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection RNN1  --concat True --sequential False --batch_size 1024 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 &&
+python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection RNN1  --concat True --sequential False --batch_size 512 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 &&
 
-python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection RNN1  --concat False --sequential False --batch_size 1024 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 &&
+python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection RNN1  --concat False --sequential False --batch_size 512 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 &&
 
-python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection RNN1  --concat True --sequential False --batch_size 1024 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 --complexity hard &&
+python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection RNN1  --concat True --sequential False --batch_size 512 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 --complexity hard &&
 
-python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection RNN1  --concat False --sequential False --batch_size 1024 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 --complexity hard &&
+python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection RNN1  --concat False --sequential False --batch_size 512 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 --complexity hard &&
 
 #the final experiment
 
-python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection CBOW  --concat False --sequential True --batch_size 1024 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 &&
+python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection CBOW  --concat False --sequential True --batch_size 512 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 &&
 
-python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection CBOW  --concat False --sequential True --batch_size 1024 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 --complexity hard &&
+python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection CBOW  --concat False --sequential True --batch_size 512 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 --complexity hard &&
 
-python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection RNN1  --concat False --sequential True --batch_size 1024 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 &&
+python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection RNN1  --concat False --sequential True --batch_size 512 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 &&
 
-python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection RNN1  --concat False --sequential True --batch_size 1024 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 --complexity hard &&
+python clean_code/main.py  --onlybin False --captions True --augment False --epochs $epochs --cosine True --projection RNN1  --concat False --sequential True --batch_size 512 --test_batch_size 32 --image_layer mlp2 --lr 0.000001 --complexity hard &&
 echo 'End'
